2197be64495b55bd6705118c376495ba0c2fbdc1
DarkPhoenix2704 <anbarasun123@gmail.com>
2023-11-04 19:41:45 +0530

    fix(web): auth

--- apps/web/pages/auth/login.tsx
+++ apps/web/pages/auth/login.tsx
@@ -7,17 +7,14 @@ const Login = () => {
 
   return (
     <>
-      <div className="flex flex-col md:flex-row h-[100vh] border-none bg-[#F5F7FA] ">
-        <div className="flex items-center justify-center h-screen ">
-          <div className="w-full border-none w-[450px] flex flex-col items-center bg-white m-8 py-8 px-8 rounded-md bg-transparent shadow-sm drop-shadow-sm">
-            <h1 className="text-3xl font-bold">Login</h1>
-
-            <Button size="sm" className="w-72 mt-2" onClick={googlelogin}>
-              <div className="flex w-full items-center justify-center gap-2 flex-row">
-                Signup with Google
-              </div>
-            </Button>
-          </div>
+      <div className="flex flex-row items-center justify-center w-full h-[100vh] border-none bg-[#F5F7FA]">
+        <div className="border-none w-[450px] flex flex-col items-center bg-white m-8 py-8 px-8 rounded-md bg-transparent shadow-sm drop-shadow-sm">
+          <h1 className="text-3xl font-bold">Login</h1>
+          <Button size="sm" className="w-72 mt-2" onClick={googlelogin}>
+            <div className="flex w-full items-center justify-center gap-2 flex-row">
+              Login
+            </div>
+          </Button>
         </div>
       </div>
     </>
