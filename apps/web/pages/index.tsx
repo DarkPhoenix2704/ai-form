4d30145d5ab477313fa0742fde9c4a9847d8fd21
DarkPhoenix2704 <anbarasun123@gmail.com>
2023-11-04 15:05:09 +0530

    Add dependencies and UI components

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -15,11 +15,6 @@ import { Button } from '@nextui-org/react';
 import { useState } from 'react';
 
 const IndexPage = () => {
-  const [text, setText] = useState('Hello World');
-  const [isChecked, setIsChecked] = useState(false);
-  const [number, setNumber] = useState(0);
-  const [date, setDate] = useState(new Date().toISOString());
-  const [selectOptions, setSelectOptions] = useState(['Hey', 'Helop']);
   return (
     <div className="flex flex-col mx-16 py-16 w-full h-full">
       <div className="flex w-full items-end">
